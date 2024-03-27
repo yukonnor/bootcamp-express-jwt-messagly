399b1d78f291856cc053ba60105ae48b5fc39a80
connor <caschm@gmail.com>
2024-03-27 09:52:02 -0400

    more starter code

--- config.js
+++ config.js
@@ -1,20 +1,19 @@
 /** Common config for message.ly */
 
 // read .env files and make environmental variables
+// NOTE: typically this wouldn't be added to the repo. Keeping for notes / reference (nothing secret here).
 
 require("dotenv").config();
 
-const DB_URI = (process.env.NODE_ENV === "test")
-  ? "postgresql:///messagely_test"
-  : "postgresql:///messagely";
+const DB_URI =
+    process.env.NODE_ENV === "test" ? "postgresql:///messagely_test" : "postgresql:///messagely";
 
 const SECRET_KEY = process.env.SECRET_KEY || "secret";
 
 const BCRYPT_WORK_FACTOR = 12;
 
-
 module.exports = {
-  DB_URI,
-  SECRET_KEY,
-  BCRYPT_WORK_FACTOR,
+    DB_URI,
+    SECRET_KEY,
+    BCRYPT_WORK_FACTOR,
 };
